15f7177cb336a85976e817dc086c7a52b6229e21
Mikhail Kiyan <kiyan@ukr.net>
2019-08-29 18:55:38 +0300

    Added ajax-request

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ Base URL - https://api.github.com/search/users?q=mkiyan
 */
 
 import { fromEvent } from 'rxjs';
-import { map, debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import {
+	map,
+	debounceTime,
+	distinctUntilChanged,
+	switchMap
+} from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax'
 
 const url = 'https://api.github.com/search/users?q='
 
@@ -13,7 +19,8 @@ const stream$ = fromEvent(search, 'input')
 	.pipe(
 		map(event => event.target.value), // получаем чистые значения из события input-а
 		debounceTime(1000), // задержка 1 секунда для события - чтобы не "дергать" API при каждом изменении input-а
-		distinctUntilChanged() // фильтруем событие потока, если значение не изменилось
+		distinctUntilChanged(), // фильтруем событие потока, если значение не изменилось
+		switchMap(value => ajax.getJSON(url + value)) // переключаемся на новый поток событий - от ajax-запроса
 	)
 
 stream$.subscribe(value => {
