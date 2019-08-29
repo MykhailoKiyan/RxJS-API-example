48a1e141129ef72388b79128246ceda271ab5342
Mikhail Kiyan <kiyan@ukr.net>
2019-08-29 18:42:15 +0300

    Added time debounce

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ Base URL - https://api.github.com/search/users?q=mkiyan
 */
 
 import { fromEvent } from 'rxjs'
-import { map } from 'rxjs/operators'
+import { map, debounceTime } from 'rxjs/operators'
 
 const url = 'https://api.github.com/search/users?q='
 
@@ -11,7 +11,8 @@ const search = document.getElementById('search')
 
 const stream$ = fromEvent(search, 'input')
 	.pipe(
-		map(event => event.target.value) // получаем чистые значения из события input-а
+		map(event => event.target.value), // получаем чистые значения из события input-а
+		debounceTime(1000) // задержка 1 секунда для события - чтобы не "дергать" API при каждом изменении input-а
 	)
 
 stream$.subscribe(value => {
