04646a2e250a0d1c6141695d076bb736682e584a
Mikhail Kiyan <kiyan@ukr.net>
2019-08-29 18:48:12 +0300

    Added distint by value of the stream

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 Base URL - https://api.github.com/search/users?q=mkiyan
 */
 
-import { fromEvent } from 'rxjs'
-import { map, debounceTime } from 'rxjs/operators'
+import { fromEvent } from 'rxjs';
+import { map, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 const url = 'https://api.github.com/search/users?q='
 
@@ -12,7 +12,8 @@ const search = document.getElementById('search')
 const stream$ = fromEvent(search, 'input')
 	.pipe(
 		map(event => event.target.value), // получаем чистые значения из события input-а
-		debounceTime(1000) // задержка 1 секунда для события - чтобы не "дергать" API при каждом изменении input-а
+		debounceTime(1000), // задержка 1 секунда для события - чтобы не "дергать" API при каждом изменении input-а
+		distinctUntilChanged() // фильтруем событие потока, если значение не изменилось
 	)
 
 stream$.subscribe(value => {
