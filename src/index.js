c6039ebe76518a78fe2f160d4dcbc8f2d2f35149
Mikhail Kiyan <kiyan@ukr.net>
2019-08-29 17:55:31 +0300

    Get clean value fron event

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ Base URL - https://api.github.com/search/users?q=mkiyan
 */
 
 import { fromEvent } from 'rxjs'
+import { map } from 'rxjs/operators'
 
 const url = 'https://api.github.com/search/users?q='
 
 const search = document.getElementById('search')
 
 const stream$ = fromEvent(search, 'input')
+	.pipe(
+		map(event => event.target.value) // получаем чистые значения из события input-а
+	)
 
 stream$.subscribe(value => {
 	console.log(value)
